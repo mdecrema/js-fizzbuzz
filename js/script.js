80d79bae239242fd09c80ba93c9bbd9153f23a27
marco <marcodecrema@libero.it>
2020-08-26 14:43:41 +0200

    prova2

--- js/script.js
+++ js/script.js
@@ -2,14 +2,13 @@ var lista=document.getElementById('lista').innerHTML;
 var listaNumeri = [];
 
 for (i=0 ; i<100; i++) {
-  lista += i;
-  if (lista % 3 == 0 && i % 5 == 0) {
+  if (i % 3 == 0 && i % 5 == 0) {
   document.getElementById('lista').innerHTML="<li>FizzBuzz</li>";
-} else if (lista % 3 == 0) {
+} else if (i % 3 == 0) {
   document.getElementById('lista').innerHTML="<li>Fizz</li>";
-} else if (lista % 5 == 0) {
+} else if (i % 5 == 0) {
   document.getElementById('lista').innerHTML="<li>Buzz</li>";
 } else {
-  document.getElementById('lista').innerHTML="<li>"+lista+"</li>";
+  document.getElementById('lista').innerHTML="<li>"+i+"</li>";
 }
 };
