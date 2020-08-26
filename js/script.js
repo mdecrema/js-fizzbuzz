d31e7bb133d060c9063e5f7cf68d5b1ce0e48419
marco <marcodecrema@libero.it>
2020-08-26 15:35:03 +0200

    commenti

--- js/script.js
+++ js/script.js
@@ -1,16 +1,19 @@
 // Richiamo il tag html per poterlo modificare
 var lista=document.getElementById('lista').innerHTML;
-var listaNumeri = [];
 
 // Ciclo for
-for (i=1 ; i<100; i++) {
+for (i = 1; i <= 100; i++) {
+  // x multipli di 3 e 5 stampo FizzBuzz
   if (i % 3 == 0 && i % 5 == 0) {
-  document.getElementById('lista').innerHTML+="<li>FizzBuzz</li>";
-} else if (i % 3 == 0) {
-  document.getElementById('lista').innerHTML+="<li>Fizz</li>";
-} else if (i % 5 == 0) {
-  document.getElementById('lista').innerHTML+="<li>Buzz</li>";
-} else {
-  document.getElementById('lista').innerHTML+="<li>"+i+"</li>";
-}
+    document.getElementById('lista').innerHTML+="<li>FizzBuzz</li>";
+  // x multipli di 3 stampo Fizz
+  } else if (i % 3 == 0) {
+    document.getElementById('lista').innerHTML+="<li>Fizz</li>";
+  // x multipli di 5 stampo Buzz
+  } else if (i % 5 == 0) {
+    document.getElementById('lista').innerHTML+="<li>Buzz</li>";
+  // x tutti gli altri numeri stampo il valore numerico
+  } else {
+    document.getElementById('lista').innerHTML+="<li>"+i+"</li>";
+  }
 };
