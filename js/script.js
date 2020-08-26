40427d72423377975d7396dc631840f6c7a629b5
marco <marcodecrema@libero.it>
2020-08-26 14:12:19 +0200

    do while

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-var listaNumeri=document.getElementById('listaNumeri');
+var lista=document.getElementById('lista');
+var check= "";
+i = 0;
+
+do {
+  check +="<li>"+i+"</li>";
+  i++;
+} while (i<=100);
+  if (i % 3 == 0) {
+    lista.innerHTML="<li>Fizz</li>";
+  } else if (i % 5 == 0) {
+    lista.innerHTML="<li>Buzz</li>";
+  } else if (i % 3 == 0 && i % 5 == 0) {
+    lista.innerHTML="<li>FizzBuzz</li>";
+  } else {
+    lista.innerHTML="<li>"+i+"</li>";
+  }
+};
